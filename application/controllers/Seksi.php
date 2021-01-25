943f9b1ccae593c98d2601bd7d0b0acb3eb074ff
MSAditama <221709797@stis.ac.id>
2021-01-25 17:23:02 +0700

    edit user access

---
+++ application/controllers/Seksi.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Seksi extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'My Profile';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/topbar', $data);
+        $this->load->view('seksi/index', $data);
+        $this->load->view('template/footer');
+    }
+}
